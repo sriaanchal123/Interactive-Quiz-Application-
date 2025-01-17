4f7385bd800b6fc20339c49ad0dd9360b42a1f60
sriaanchal123 <srivastavaanchal2006@gmail.com>
2025-01-17 18:31:32 +0530

    Create question.js

---
+++ question.js
@@ -0,0 +1,59 @@
+const questions=[
+{
+    numb:1,
+    question:"What does HTML stands for?",
+    options:[
+        "Hyper Text pre processor",
+        "Hyper text Markup language",
+        "Hyper text multiple language",
+         "Hyper Tool Multi language"
+        ],
+        answer:"Hyper text Markup language",
+    },
+        {
+            numb:2,
+           question:"What does CSS stands for?" ,
+           options:[
+            "Cascading stylesheets",
+            "Colorful stylesheets",
+            "computer stylesheets",
+            "Cascading simplesheets"
+           ],
+           answer:"Cascading stylesheets",     
+        },
+        {
+            numb:3,
+            question:"Which data structure is best for searching elements quickly?",
+            options:[
+                "Binary search Tree",
+                "Array",
+                "Linked list",
+                "Queue"
+            ],
+            answer:"Binary search Tree",
+
+        },
+        {
+            numb:4,
+            question:"Which year does javascript launced?",
+            options:[
+                "1996",
+                "1995",
+                "1994",
+                "none of these"
+        ],
+        answer:"1995",
+        },
+        {
+            numb:5,
+            question:"In Javascript ,Which of the following is a logical operator?",
+            options:[
+                "|",
+                "&&",
+                "%",
+                "/"
+            ],
+            answer:"&&",
+        },
+];
+
